76e74c8016da6fecbf14f0fe802a471a1b7bf697
Shahidnsu <shahid.khan@northsouth.edu>
2023-01-10 14:51:14 +0600

    from validation done from the frontend side

--- client/src/app/application-from/application-from.component.ts
+++ client/src/app/application-from/application-from.component.ts
@@ -1,21 +1,31 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-application-from',
   templateUrl: './application-from.component.html',
   styleUrls: ['./application-from.component.css'],
 })
 export class ApplicationFromComponent {
+  constructor(private Builder: FormBuilder) { }
+
   ApplicantForm = this.Builder.group({
-    firstName: [''],
-    lastName: [''],
-    Email: [''],
-    phoneNumber: [''],
-    githubUserName: [''],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    Email: ['', Validators.required],
+    phoneNumber: ['', Validators.required],
+    githubUserName: ['', Validators.required],
   });
 
-  constructor(private Builder: FormBuilder) { }
   onSubmit() {
     console.warn(this.ApplicantForm.value);
+  }
+
+  get ApplicantFormControl() {
+    return this.ApplicantForm.controls;
   }
 }
