06522764b5bca0dd6c8453816ab19e0c4d511a57
Shahidnsu <shahid.khan@northsouth.edu>
2023-01-15 21:41:21 +0600

    remvoed import statements for navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { LoginPageComponent } from './Admin/login-page/login-page.component';
 import { SucessPageComponent } from './Apply/application-from/sucess-page/sucess-page.component';
 import { DashboardComponent } from './Dashborad/dashboard/dashboard.component';
 import { UserTableComponent } from './Dashborad/dashboard/user-table/user-table.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
 import { SingleUserComponent } from './Dashborad/dashboard/single-user/single-user.component';
 import { UserLineChartComponent } from './Dashborad/dashboard/user-line-chart/user-line-chart.component';
 import { JwtHelperService,JWT_OPTIONS } from '@auth0/angular-jwt';
@@ -24,7 +24,7 @@ import { ErrorComponent } from './error/error.component'
     SucessPageComponent,
     DashboardComponent,
     UserTableComponent,
-    NavbarComponent,
+   
     SingleUserComponent,
     UserLineChartComponent,
     ErrorComponent,
